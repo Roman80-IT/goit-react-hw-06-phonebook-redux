dbf8f96baa52ea4a0de35204fef1623eb840ab45
Roman <jrj80mail@gmail.com>
2023-11-03 19:01:44 +0200

    Update store.js

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,11 +1,9 @@
 import { devToolsEnhancer } from '@redux-devtools/extension';
 import { createStore, combineReducers } from 'redux';
-// import { contactsOperationReducer } from './contactsOperationReducer';
-
-// 53:30   State завжди задавати як об’єкт, щоб не було багів під час синхронізації з локальним сховищем
+import { contactsOperationReducer } from './contactsOperationReducer';
 
 const rootReducer = combineReducers({
-  contactsOperation: {},
+  contactsOperation: contactsOperationReducer,
   //   contacts: contactsReducer,
   //   filter: filterReducer,
 });
